e4da6e2dd84ead7df818b6f25e0cdeaf50fa9de7
ELIJAHADJEI <eadjei128@st.ug.edu.gh>
2024-05-19 16:54:36 +0000

    DCIT202_Task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,9 +1,9 @@
-function processArray(arr) {
-    return arr.map(num => {
-        if (num % 2 === 0) {
-            return num * num; // Square the even numbers
-        } else {
-            return num * 3;   // Triple the odd numbers
-        }
+function formatArrayStrings(strArray, numArray) {
+    return strArray.map((str, i) => {
+      if (numArray[i] % 2 === 0) {
+        return str.toUpperCase();
+      } else {
+        return str.toLowerCase();
+      }
     });
-}
+  }
